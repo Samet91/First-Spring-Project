5e583240e6ca79ed9643e2d6a83d685cb3586b70
Samet Uslu <samet_u@hotmail.de>
2022-02-15 18:35:19 +0100

    initial commit

---
+++ src/main/java/de/neuefische/firstspringbootproject/student/StudentController.java
@@ -0,0 +1,32 @@
+package de.neuefische.firstspringbootproject.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+    @GetMapping("/{id}")
+    public Student getThisStudent(@PathVariable String id) {
+        return studentService.getId(id);
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudents(student);
+    }
+
+    @GetMapping()
+    public List<Student> allStudent() {
+        return studentService.getStudents();
+    }
+}
